7203c0517b01fb786379dd82a838a620a2c47b2e
Emil VATAI <emil.vatai@gmail.com>
2026-04-03 18:55:03 +0900

    Get ML4T running

--- app.py
+++ app.py
@@ -1,11 +1,79 @@
 #!/usr/bin/env python
+import os
+import re
+import sys
+from typing import Optional
 
 from tadashi.apps import App
+from tadashi.translators import Polly, Translator
+
+ml4tadashi = os.path.dirname(__file__)
+ml4tadashi = os.path.dirname(ml4tadashi)
+ml4tadashi = os.path.dirname(ml4tadashi)
+ml4tadashi = os.path.dirname(ml4tadashi)
+sys.path.append(ml4tadashi)
+ml4tadashi = os.path.join(ml4tadashi, "ML4TADASHI")
+sys.path.append(ml4tadashi)
+
+import ML4TADASHI
 
 
 class Sw4Lite(App):
     def __init__(
         self,
-        source="ew-cfromfort.C",
+        source="src/ew-cfromfort.C",
+        translator: Optional[Translator] = None,
+        compiler_options: list = None,
+        ephemeral: bool = False,
+        populate_scops: bool = True,
     ):
-        pass
+        super().__init__(
+            source=source,
+            translator=translator,
+            compiler_options=compiler_options,
+            ephemeral=ephemeral,
+            populate_scops=populate_scops,
+        )
+
+    def codegen_init_args(self):
+        return {}
+
+    def app_required_options(self) -> list[str]:
+        return ["-Isrc/double"]
+
+    def compile_cmd(self, suffix: str) -> list[str]:
+        cmd = [
+            "make",
+            "-j",
+            "ckernel=yes",
+            f"SOURCE={self.source.with_suffix('').name}",
+            f"APP={self.output_binary.name}",
+        ]
+        return cmd
+
+    def run_cmd(self) -> list[str]:
+        cmd = [
+            f"./optimize_c/{self.output_binary.name}",
+            "tests/pointsource/pointsource.in",
+        ]
+        return cmd
+
+    def extract_runtime(self, proc: CompletedProcess) -> float:
+        stdout = proc.stdout.decode()
+        print(stdout)
+        lines = stdout.split("\n")
+        pattern = re.compile(r" Total running time:.*(\d+\.\d+).*")
+        for line in lines:
+            match = pattern.match(line)
+            if match:
+                return float(match.groups()[0])
+        raise Exception("No output found")
+
+
+def main():
+    app = Sw4Lite(translator=Polly("clang"))
+    ML4TADASHI.run(Sw4Lite, {"translator": "Polly"})
+
+
+if __name__ == "__main__":
+    main()
